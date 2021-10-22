c9a28b2d717b256523f476783d2a9ccc61a27a27
Ronald Head <ronaldlhead@gmail.com>
2021-10-22 10:38:35 -0600

    Create new airport object and key-variable pairs

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var newAirport = {
+  name: "Denver International Airport",
+  location: "Denver",
+  numTerminals: 50,
+  numAirlines: 20,
+  isActive: true,
+  isInactive: false,
+  vehicleRentals: ["Hertz", "Enterprise", "Budget"],
+  airlineNames: ["Delta", "United", "Spirit", "Southwest"]
+}
