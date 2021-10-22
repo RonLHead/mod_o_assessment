97a79e1d901d95b1aaf551517a08a20db32f7db5
Ronald Head <ronaldlhead@gmail.com>
2021-10-22 10:43:35 -0600

    Add new vehicle and key-value pairs

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var newVehicle = {
+  make: "Subaru",
+  model: "Forester",
+  miles: 5700,
+  weightInLbs: 3500,
+  isRegistered: true,
+  isInsured: true,
+  addFeatures: ["GPS", "Bluetooth", "SirusXM", "Rear View Camera"],
+  standardFeatures: ["4-Wheel Drive", "AC/Heat", "Adjustable Seats"]
+}
